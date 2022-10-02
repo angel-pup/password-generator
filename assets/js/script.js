3e48a5d201d4cdaa8bbaca09a9fc5d9ab726a9ca
Spencer Stewart <r.spencer.stewart@gmail.com>
2022-10-01 21:07:41 -0500

    improved flow of code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,46 @@
   // Assignment code here
+
+  // Initialize constant arrays, for picking characters later on
   const upperAlpha = [...Array(26)].map((_, i) => String.fromCharCode(i + 65));
   const lowerAlpha = [...Array(26)].map((_, i) => String.fromCharCode(i + 97));
   const numbers = [...Array(10)].map((_, i) => i);
   const symbols = "!\"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~".split("");
-  console.log(upperAlpha, lowerAlpha, numbers, symbols);
 
+  /**
+   * Prompts user for length of password and the types of characters they wish to include in the password
+   * @returns {string} returns randomly generated password
+   */
   function generatePassword() {
+    // Declare local variables, initialize most
     let invalid = true;
     let passLength;
-    let options;
-    let alertMessage;
+    let options = [];
+    let alertMessage = "";
     let password = "";
 
-    while(invalid) {
 
-      alertMessage = "";
-      options = [];
+    // Prompt user for length of password
+    passLength = prompt("Length of password?");
 
-      passLength = prompt("Length of password?");
+    // Prepare alert message if password length is invalid
+    isNaN(passLength) || passLength < 8 || passLength > 128 ?
+        alertMessage = "Password length must be a valid number between 7 and 129." : invalid = false;
 
-      if (isNaN(passLength)) {
-        alertMessage += "Password length given is not a number.\n";
-      } else if (passLength < 8 || passLength > 128) {
-        alertMessage += "Password length must be between 7 and 129 characters long.\n";
-      } else {
-        invalid = false;
-      }
+    // Do not prompt user further unless prior conditions are met
+    if (!invalid) {
 
+      // Prompt user for types of characters they wish to use, log which characters go unused
       confirm("Use UPPERCASE letters?") ? options.push(0) : console.log("No UPPERCASE characters.");
       confirm("Use lowercase letters?") ? options.push(1) : console.log("No lowercase letters.");
       confirm("Use Numb3rs?") ? options.push(2) : console.log("No Numb3rs.");
       confirm("Use $ymbols?") ? options.push(3) : console.log("No $ymbols.");
 
+      // Ensure at least one character type has been chosen, prepare alert if not
       if (options.length !== 0) {
+        // Pick characters until password length condition is satisfied
         for (let i = 0; i < passLength; i++) {
 
+          // Choose character type at random, then choose character of that type at random
           switch (options[Math.floor(Math.random() * options.length)]) {
             case 0:
               password += upperAlpha[Math.floor(Math.random() * upperAlpha.length)];
@@ -49,31 +55,34 @@
               password += symbols[Math.floor(Math.random() * symbols.length)];
               break;
             default:
-              console.error("Switch broke from withing password generation function");
-          }
-        }
+              // Do nothing; Code execution should never reach here
 
+          }
+
+        }
+        // Return generated password
         return password;
 
       } else {
-        invalid = true;
-        alertMessage += "Please choose at least one kind of character of which to generate the password.";
+        alertMessage += "Please choose at least one kind of character of which to generate the password with."
+
       }
 
-      alert(alertMessage);
-
     }
+    // Will reach alert if password conditions aren't met, returns appropriate alert message
+    alert(alertMessage);
+
   }
 
   // Get references to the #generate element
-  var generateBtn = document.querySelector("#generate");
+  const generateBtn = document.querySelector("#generate");
 
   // Write password to the #password input
   function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    const password = generatePassword();
+    const passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+    passwordText.value = password;
 
   }
 
